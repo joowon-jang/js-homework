e6a3d31ac58edace6a3c8d3b5a296a6d7a8aa56c
JoowonJang <wndnjs5248@hanmail.net>
2024-06-08 22:29:55 +0900

    Modify Mission-0

--- mission00/main.js
+++ mission00/main.js
@@ -1,4 +1,6 @@
 function getValueAtObject(obj, key) {
+  // 구글에 검색해보고 썼는데 이거랑 차이가 있나요?
+  // Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === "object"
   if (obj.constructor !== Object) {
     throw new Error("대상이 객체가 아닙니다.");
   }
@@ -10,7 +12,11 @@ function getValueAtObject(obj, key) {
 }
 
 function getNumberAtArray(arr, index) {
-  if (!Array.isArray(arr)) return;
+  if (!Array.isArray(arr)) {
+    throw new Error("대상이 배열이 아닙니다.");
+  }
   if (index >= 0 && index < arr.length) return arr[index];
   else throw new Error("유효한 index가 아닙니다.");
 }
+
+getValueAtObject(person, "name");
